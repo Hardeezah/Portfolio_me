16042ebcf4a5ceedfb8a2ee4cbf046b69ab7ed11
Hardeezah <67266423+Hardeezah@users.noreply.github.com>
2023-07-28 17:14:39 +0100

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,122 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  sumr,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  }
+];
+
+const experiences = [
+  
+];
+
+const testimonials = [
+];
+
+const projects = [
+  {
+    name: "Sumr | Article Summarizer",
+    description:
+      "Sumr is a web application that summarizes articles from any web-link. It uses the power of OPEN AI's GPT-3 to summarize articles.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "redux toolkit",
+        color: "green-text-gradient",
+      },
+      {
+        name: "material-ui",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: sumr,
+    source_code_link: "https://github.com/Hardeezah/openai-summarizer",
+  }
+];
+
+export { services, technologies, experiences, testimonials, projects };
